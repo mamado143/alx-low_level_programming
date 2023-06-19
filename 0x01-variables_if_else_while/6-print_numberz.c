7c4acf1e3607c9a05d64aca94f2c1ce2fc109104
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-20 00:00:51 +0300

    add  6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Desription - Write a program prints all digit numbers of base 10 starting from 0
+ *Return: Always return 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar ('0' + n);
+	}
+	putchar ('\n');
+}
