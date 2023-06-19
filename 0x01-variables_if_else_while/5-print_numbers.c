6e8e2379171784ed795cd048bd3c48acd97d56d9
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-19 23:38:48 +0300

    add file 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - Entry main
+  *Description - Write a program to print all degits from 0..10
+  *Return: Always return 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	return (0);
+}
