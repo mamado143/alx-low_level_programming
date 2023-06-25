af8efd701e24256d29558c54d117655978c55a0f
mamado143 <midodahir123@gmail.com>
2023-06-25 03:31:51 -0700

    see if it's fixed the check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
   *_isupper - is a function which turn characters to upperCase.
   *@c: calculates the character A and a.
   *Return: return 1 or 0
   */
+
 int _isupper(int c)
 {
 	if (c <= 65 && c <= 98)
