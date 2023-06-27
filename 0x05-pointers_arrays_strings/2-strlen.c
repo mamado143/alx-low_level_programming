4ac074deaa6875eb99d1898176e7a159bd65f000
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-27 14:03:05 +0300

    add file 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - is a function which founds the length of character
+ *@s: is a pointer varible
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
