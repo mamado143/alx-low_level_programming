776a57b53a660d46eb21d5ab777de2d4f9c4d37b
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-27 20:55:18 +0300

    3-puts file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to the standard output.
+ * @str: The string to be printed.
+ * Return: return void.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
