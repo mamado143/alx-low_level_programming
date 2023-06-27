9ccbd8eb17273b58b031b4d0de797f81fd50520a
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-28 02:20:28 +0300

    add file 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		printf("%d", a[i]);
-		if (i != n - 1)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		printf("\n");
+		return;
 	}
-	_putchar('\n');
+	printf("%d", a[0]);
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", a[i]);
+	}
+	printf("\n");
 }
