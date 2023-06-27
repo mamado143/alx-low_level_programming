992cb99ed91e557f4acdcd82d178d9eacf466f92
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-28 00:43:29 +0300

    see if it's fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,9 @@ void rev_string(char *s)
 
 	for (i = 0; i < length / 2; i++)
 	{
-		char temp = s[i];
+		char temp;
+		
+		temp = s[i];
 		s[i] = s[length - i - 1];
 		s[length - i - 1] = temp;
 	}
