a92cc9e7189b28fbdc2db218e2c1025669162f03
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-06-29 11:33:47 +0300

    add file 100-atowi.c)

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - is a function to convert a string.
+ * @s: is string input parameter
+ * Return: Always 0
+*/
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
+}
+
