885ad430253715cbe5b807d64f002120636be6a0
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-04 09:57:03 +0300

    add strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The characters to search for.
+ *
+ * Return: Pointer to the byte in `s` that matches `accept`.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
