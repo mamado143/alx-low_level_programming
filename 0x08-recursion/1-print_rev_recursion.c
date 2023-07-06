1c161a459c46b143406a0dabebd18b8c2cfa5a13
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-06 08:41:33 +0300

    add  1-puts_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+ * _print_rev_recursion - This function is printing reverse string.
+ * @s: is a pointer to character.
+ * Return: return a pointer reserved.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+}
