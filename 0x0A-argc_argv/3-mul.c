dffdaeb640b08580c5a22e3c9ef7775adf6b8b38
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-08 11:50:09 +0300

    add  1-args.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.c"
+#include <stdlib.h>
+/**
+  * main - An Entry point funciton to print multiplication two argument.
+  * @argc: The count of the arguments.
+  * @argv: The vector of arguents count.
+  * Return: Always return 0 or 1.
+  */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	num 1 = atoi(argv[1];
+	num 2 = atoi(argv[2];
+	result = num1 * num1;
+	printf("%d\n", result);
+	return (0);
+}
