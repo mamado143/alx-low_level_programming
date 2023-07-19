a59560b5fae9fb224435a234a32d08fea592851d
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-19 14:17:06 +0300

    modify file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /**
-  dog_t - is a typedef for sturct dog
+  * dog_t - is a typedef for sturct dog
   */
 typedef struct dog dog_t;
 #endif /* DOG_H */
