1b3d291825730976b15ee04ac6f3a40c20c8dbff
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-29 20:37:34 +0300

    file 0_print_lists.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
-
-	while (h != NULL)
+		while (h)
 	{
-		printf("[%u] %s\n", h->len, (h->str != NULL) ? h->str : "(nil)");
-		node_count++;
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		node_count++;
 	}
+
 	return (node_count);
 }
