aaae1dac81cb4d4d4ba8582e912242fe8f6a3cba
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-30 11:31:31 +0300

    add file 1.list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,17 @@
+#include "lists.h"
+
 size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h)
+	{
+		if (!h->len)
+			printf("[0] (nil)");
+		else
+		h = h->next;
+		node_count++;
+	}
+	return (node_count++);
+}
+
