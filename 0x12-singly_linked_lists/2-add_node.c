4eae77ae0fa43c58bfb034fbc1bd90709a51af1e
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-30 19:49:21 +0300

    add file 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,11 +8,17 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node =  malloc(sizeof(list_t));
+	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	new_node =  malloc(sizeof(list_t));
 		if (new_node == NULL)
 			return (NULL);
 	new_node->str = strdup(str);
+	new_node->len = len;
 	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	(*head) = new_node;
+	return (*head);
 }
