2b2a0f1a054e7cc91762cd89072a2b45d4877890
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-30 11:37:56 +0300

    add file 1.list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+  *list_len - this function is returning the number of elements in list.
+  * @h: is the head of the linkedlist.
+  *
+  *Return: The number of elements in the linked list.
+  */
 size_t list_len(const list_t *h)
 {
 	size_t node_count = 0;
