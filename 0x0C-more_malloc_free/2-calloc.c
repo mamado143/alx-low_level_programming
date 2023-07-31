d0be94db95030c2e52f7c5413b6ca57877036aba
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-31 18:21:50 +0300

    add 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,10 +14,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	total_size = nmemb * size;
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
-	ptr(malloc(total_size);
+	}
+	ptr = malloc(total_size);
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 	memset(ptr, 0, total_size);
 	return (ptr);
 }
