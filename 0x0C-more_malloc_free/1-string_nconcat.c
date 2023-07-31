89aaec11e653b361d6a8cb5d6bf8af7c7ad6b90d
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-31 12:12:47 +0300

    add string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,32 @@
 #include "main.h"
 /**
   * string_nconcat  - is a function to concatnate two strings.
-  * @s1: is a pointer to string to concatnate.
+  * @s1: is a pointer to string to appent to
   * @s2: is a pointer to string to concatnate from.
-  * @n: is unsigned integer.
+  * @n: is unsigned integer of number of byts from s2 to s1.
   *
-  * Return: It returns the results of the concatinations.
+  * Return: It returns the results pointer of the concatinations.
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t len1 = 0;
-	size_t len2 = 0;
-	size_t copy_len;
 	char *result = NULL;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	copy_len = (n >= len2) ? len2 : n;
-	result = (char *)malloc((len1 + copy_len + 1) * sizeof(char));
-	strcpy(result, s1);
-	strncat(result, s2, copy_len);
-	result[len1 + copy_len] = '\0';
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
+	if (n < len2)
+		result = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		result = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!result)
+		return (NULL);
+	for (; i < len1; i++)
+		result[i] = s1[i];
+	for (; n < len2 && i < (len1 + n); i++, j++)
+		result[i] = s2[j];
+	for (; n >= len2 && i < (len1 + len2); i++, j++)
+		result[i] = s2[j];
+	result[i] = '\0';
 	return (result);
 }
