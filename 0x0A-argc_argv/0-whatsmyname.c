521722350194df7cae4f979998cdfa3a2250fb1a
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-09-02 10:32:54 +0300

    file 0-2hatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry function about printing program name.
+  * @argc: int
+  * @argv: array pointer char
+  * Return: returns 0.
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
