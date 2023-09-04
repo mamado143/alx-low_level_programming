cc49c71f3c509cf3baadc2cdbdccf7718da365e2
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-09-04 17:49:48 +0300

    modify file 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
-	if (size == 0 || size == 0)
+	if (size == 0 || arr == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
