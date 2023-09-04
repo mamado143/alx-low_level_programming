92ceb099c0ca336f59f8ce41bd00b9fc5c37e231
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-09-04 10:48:42 +0300

    add file args 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Main entry of the program.
+  * @argc: is the arguments.
+  * @argv: is an array of arguments.
+  * Return: returns 0 or 1.
+  */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc > 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
